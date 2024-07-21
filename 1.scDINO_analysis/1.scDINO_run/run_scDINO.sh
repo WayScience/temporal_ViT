7f969c07e2ee7250b9e182237e836c7281778c42
MikeLippincott <1michaell2017@gmail.com>
2024-07-21 15:58:30 -0600

    rerpocess a larger batch

--- 1.scDINO_analysis/1.scDINO_run/run_scDINO.sh
+++ 1.scDINO_analysis/1.scDINO_run/run_scDINO.sh
@@ -8,9 +8,20 @@ echo "Starting the scDINO analysis..."
 # make logs directory if it doesn't exist
 mkdir -p logs
 
-snakemake -s only_downstream_snakefile --configfile="only_downstream_analyses.yaml" --until compute_CLS_features --cores 2 --rerun-incomplete > logs/CLS_token_run.log 2>&1 > logs/CLS_token_run.log 2>&1
 
-snakemake -s only_downstream_snakefile --configfile="only_downstream_analyses.yaml" --cores all --rerun-incomplete > logs/downstream_run.log 2>&1
+
+# calculate the number of cores this machine has and use all but 2
+# unless it has only 2 cores, in which case use 1 core
+NCORES=$(nproc)
+if [ $NCORES -gt 2 ]; then
+    NCORES=$(($NCORES - 2))
+else
+    NCORES=1
+fi
+
+snakemake -s only_downstream_snakefile --configfile="only_downstream_analyses.yaml" --until compute_CLS_features --cores $NCORES --rerun-incomplete > logs/CLS_token_run.log 2>&1 > logs/CLS_token_run.log 2>&1
+
+snakemake -s only_downstream_snakefile --configfile="only_downstream_analyses.yaml" --cores $NCORES --rerun-incomplete > logs/downstream_run.log 2>&1
 
 echo "scDINO analysis complete"
 
